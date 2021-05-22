f516059d4b9784e53ad2ff115332185bf33e2788
Filipe dos Santos <42256808+filipe-git@users.noreply.github.com>
2021-05-22 15:48:31 -0300

    Update ContaPessoas.py

--- ContaPessoas.py
+++ ContaPessoas.py
@@ -1,134 +1,130 @@
+import os
 import datetime
 import math
 import cv2
 import numpy as np
 
-#variaveis globais
-width = 0
-height = 0
-ContadorEntradas = 0
-ContadorSaidas = 0
-AreaContornoLimiteMin = 3000  #este valor eh empirico. Ajuste-o conforme sua necessidade
-ThresholdBinarizacao = 70  #este valor eh empirico, Ajuste-o conforme sua necessidade
-OffsetLinhasRef = 150  #este valor eh empirico. Ajuste- conforme sua necessidade.
+# Variaveis globais
+largura = 0
+altura = 0
+contaEntrada = 0
+contaSaida = 0
+AreaContornoLimiteMin = 3000  # Este valor pode ser alterado
+ThresholdBinarizacao = 70  # Este valor pode ser alterado
+OffsetLinhasRef = 150  # Este valor pode ser alterado
 
-#Verifica se o corpo detectado esta entrando da sona monitorada
-def TestaInterseccaoEntrada(y, CoordenadaYLinhaEntrada, CoordenadaYLinhaSaida):
-        DiferencaAbsoluta = abs(y - CoordenadaYLinhaEntrada)
+# Backup do arquivo dados anterior
+os.system("cp dados.txt dados-backup.txt")
+# Cria um novo arquivo de dados
+os.system("> dados.txt")
 
-        if ((DiferencaAbsoluta <= 2) and (y < CoordenadaYLinhaSaida)):
-                return 1
-        else:
-                return 0
+# Verifica se o corpo detectado esta entrando da area monitorada
+def testaIntersecaoEntrada(y, coordenadaYLinhaEntrada, coordenadaYLinhaSaida):
+    diferencaAbsoluta = abs(y - coordenadaYLinhaEntrada)
+    if ((diferencaAbsoluta <= 2) and (y < coordenadaYLinhaSaida)):
+        return 1
+    else:
+        return 0
 
-#Verifica se o corpo detectado esta saindo da sona monitorada
-def TestaInterseccaoSaida(y, CoordenadaYLinhaEntrada, CoordenadaYLinhaSaida):
-        DiferencaAbsoluta = abs(y - CoordenadaYLinhaSaida)
-
-        if ((DiferencaAbsoluta <= 2) and (y > CoordenadaYLinhaEntrada)):
-                return 1
-        else:
-                return 0
+# Verifica se o corpo detectado esta saindo da area monitorada
+def testaInterseccaoSaida(y, coordenadaYLinhaEntrada, coordenadaYLinhaSaida):
+    diferencaAbsoluta = abs(y - coordenadaYLinhaSaida)
+    if ((diferencaAbsoluta <= 2) and (y > coordenadaYLinhaEntrada)):
+        return 1
+    else:
+        return 0
 
 camera = cv2.VideoCapture(0)
 
-#forca a camera a ter resolucao 640x480
-camera.set(3,640)
-camera.set(4,480)
+# Forca resolucao de 1024x768 na camera
+camera.set(3,1024)
+camera.set(4,768)
 
-PrimeiroFrame = None
+primeiroFrame = None
 
-#faz algumas leituras de frames antes de consierar a analise
-#motivo: algumas camera podem demorar mais para se "acosumar a luminosidade" quando ligam, capturando frames consecutivos com muita variacao de luminosidade. Para nao levar este efeito ao processamento de imagem, capturas sucessivas sao feitas fora do processamento da imagem, dando tempo para a camera "se acostumar" a luminosidade do ambiente
-
+# Realiza algumas leituras antes de iniciar a analise a fim da camera se acostumar com a luminosidade do local
 for i in range(0,20):
     (grabbed, Frame) = camera.read()
 
 while True:
-    #le primeiro frame e determina resolucao da imagem
+    # Le o primeiro frame e determina resolucao da imagem
     (grabbed, Frame) = camera.read()
-    height = np.size(Frame,0)
-    width = np.size(Frame,1)
+    altura = np.size(Frame,0)
+    largura = np.size(Frame,1)
 
-    #se nao foi possivel obter frame, nada mais deve ser feito
+    # Se nao foi possivel obter frame, nada mais deve ser feito
     if not grabbed:
         break
 
-    #converte frame para escala de cinza e aplica efeito blur (para realcar os contornos)
-    FrameGray = cv2.cvtColor(Frame, cv2.COLOR_BGR2GRAY)
-    FrameGray = cv2.GaussianBlur(FrameGray, (21, 21), 0)
+    # Converte frame para escala de cinza e realca os contornos
+    frameGray = cv2.cvtColor(Frame, cv2.COLOR_BGR2GRAY)
+    frameGray = cv2.GaussianBlur(frameGray, (21, 21), 0)
 
-    #como a comparacao eh feita entre duas imagens subsequentes, se o primeiro frame eh nulo (ou seja, primeira "passada" no loop), este eh inicializado
-    if PrimeiroFrame is None:
-        PrimeiroFrame = FrameGray
+    # Como a comparacao eh feita entre duas imagens subsequentes, inicializa o primeiro frame se ele for nulo
+    if primeiroFrame is None:
+        primeiroFrame = frameGray
         continue
-
-    #ontem diferenca absoluta entre frame inicial e frame atual (subtracao de background)
-    #alem disso, faz a binarizacao do frame com background subtraido
-    FrameDelta = cv2.absdiff(PrimeiroFrame, FrameGray)
+    
+    FrameDelta = cv2.absdiff(primeiroFrame, frameGray)
     FrameThresh = cv2.threshold(FrameDelta, ThresholdBinarizacao, 255, cv2.THRESH_BINARY)[1]
-
-    #faz a dilatacao do frame binarizado, com finalidade de elimunar "buracos" / zonas brancas dentro de contornos detectados.
-    #Dessa forma, objetos detectados serao considerados uma "massa" de cor preta
-    #Alem disso, encontra os contornos apos dilatacao.
     FrameThresh = cv2.dilate(FrameThresh, None, iterations=2)
     _, cnts, _ = cv2.findContours(FrameThresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    QtdeContornos = 0
+    contaContornos = 0
 
-    #desenha linhas de referencia
-    CoordenadaYLinhaEntrada = (height / 2)-OffsetLinhasRef
-    CoordenadaYLinhaSaida = (height / 2)+OffsetLinhasRef
-    cv2.line(Frame, (0,CoordenadaYLinhaEntrada), (width,CoordenadaYLinhaEntrada), (255, 0, 0), 2)
-    cv2.line(Frame, (0,CoordenadaYLinhaSaida), (width,CoordenadaYLinhaSaida), (0, 0, 255), 2)
+    # Desenha linhas de referencia 
+    coordenadaYLinhaEntrada = (altura / 2)-OffsetLinhasRef
+    coordenadaYLinhaSaida = (altura / 2)+OffsetLinhasRef
+    cv2.line(Frame, (0,coordenadaYLinhaEntrada), (largura,coordenadaYLinhaEntrada), (255, 0, 0), 2)
+    cv2.line(Frame, (0,coordenadaYLinhaSaida), (largura,coordenadaYLinhaSaida), (0, 0, 255), 2)
 
 
-    #Varre todos os contornos encontrados
+    # Percorre todos os contornos encontrados
     for c in cnts:
-        #contornos de area muto pequena sao ignorados.
+        # Ignora os contornos de areas muito pequenas
         if cv2.contourArea(c) < AreaContornoLimiteMin:
             continue
 
-        #Para fins de depuracao, contabiliza numero de contornos encontrados
-        QtdeContornos = QtdeContornos+1
+        # Contabiliza os contornos encontrados para fins de depuracao
+        contaContornos = contaContornos+1    
 
-        #obtem coordenadas do contorno (na verdade, de um retangulo que consegue abrangir todo ocontorno) e
-        #realca o contorno com um retangulo.
+        # Obtem coordenadas do contorno e realca o contorno com um retangulo
         (x, y, w, h) = cv2.boundingRect(c) #x e y: coordenadas do vertice superior esquerdo
                                            #w e h: respectivamente largura e altura do retangulo
 
         cv2.rectangle(Frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-        #determina o ponto central do contorno e desenha um circulo para indicar
-        CoordenadaXCentroContorno = (x+x+w)/2
-        CoordenadaYCentroContorno = (y+y+h)/2
-        PontoCentralContorno = (CoordenadaXCentroContorno,CoordenadaYCentroContorno)
-        cv2.circle(Frame, PontoCentralContorno, 1, (0, 0, 0), 5)
-
-        #testa interseccao dos centros dos contornos com as linhas de referencia
-        #dessa forma, contabiliza-se quais contornos cruzaram quais linhas (num determinado sentido)
-        if (TestaInterseccaoEntrada(CoordenadaYCentroContorno,CoordenadaYLinhaEntrada,CoordenadaYLinhaSaida)):
-            ContadorEntradas += 1
-
-        if (TestaInterseccaoSaida(CoordenadaYCentroContorno,CoordenadaYLinhaEntrada,CoordenadaYLinhaSaida)):
-            ContadorSaidas += 1
-
-    #Teste para salvar em arquivo
-    arquivo = open("dados.txt", "a")
-    arquivo.write(str(ContadorEntradas) + ";" + str(ContadorSaidas) + ";\n")
+        # Desenha um circulo para indicar o ponto central do contorno
+        coordenadaXCentroContorno = (x+x+w)/2
+        coordenadaYCentroContorno = (y+y+h)/2
+        pontoCentralContorno = (coordenadaXCentroContorno,coordenadaYCentroContorno)
+        cv2.circle(Frame, pontoCentralContorno, 1, (0, 0, 0), 5)
         
-    #print "Contornos encontrados: "+str(QtdeContornos)
-    print "Contornos: "+str(QtdeContornos)+" Entradas: "+str(ContadorEntradas)+" Saidas: "+str(ContadorSaidas)
+    # Verifica a intersecao dos centros dos contornos com as linhas de entrada e saida e contabiliza quantas pessoas entraram no local
+	if (testaIntersecaoEntrada(coordenadaYCentroContorno,coordenadaYLinhaEntrada,coordenadaYLinhaSaida)):
+            contaEntrada += 1
+            arquivo = open("dados.txt", "a")
+            arquivo.write(str(contaEntrada) + ";" + str(contaSaida) + ";\n")
+            arquivo.close()
 
-    #Escreve na imagem o numero de pessoas que entraram ou sairam da area vigiada
-    cv2.putText(Frame, "Entradas: {}".format(str(ContadorEntradas)), (10, 50),
+    # Verifica a intersecao dos centros dos contornos com as linhas de entrada e saida e contabiliza quantas pessoas sairam no local
+	if (testaInterseccaoSaida(coordenadaYCentroContorno,coordenadaYLinhaEntrada,coordenadaYLinhaSaida)):  
+            contaSaida += 1
+            arquivo = open("dados.txt", "a")
+            arquivo.write(str(contaEntrada) + ";" + str(contaSaida) + ";\n")
+            arquivo.close()
+    
+    # Imprime em tela as informacoes de entrada e saida
+    #print "Contornos: "+str(contaContornos)+" Entradas: "+str(contaEntrada)+" Saidas: "+str(contaSaida)
+    
+    # Escreve na imagem o numero de pessoas que entraram ou sairam. Se deixar comentada as proximas 6 linhas, o programa podera ser executado via ssh tambem
+    cv2.putText(Frame, "Entradas: {}".format(str(contaEntrada)), (10, 50),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (250, 0, 1), 2)
-    cv2.putText(Frame, "Saidas: {}".format(str(ContadorSaidas)), (10, 70),
+    cv2.putText(Frame, "Saidas: {}".format(str(contaSaida)), (10, 70),
                 cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
     cv2.imshow("Trabalho de Sistemas Microprocessados Avancados", Frame)
     cv2.waitKey(1);
 
-
-#fecha a câmera e todas janelas abertas
+# Limpa a camera e fecha as janelas abertas
 camera.release()
 cv2.destroyAllWindows()
